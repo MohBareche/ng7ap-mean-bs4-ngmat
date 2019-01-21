611a692e4b0de7da8e04035f2c2ed3b0a062ed57
Moh Bareche <mbareche.bic@gmail.com>
2019-01-21 11:31:01 -0500

    ng7 router bs4 ang-mat-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { ExempleComponent } from './components/exemple/exemple.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ListComponent } from './components/list/list.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'exemple', component: ExempleComponent },
-  { path: '', redirectTo: 'exemple', pathMatch: 'full' }
+  { path: 'create', component: CreateComponent },
+  { path: 'edit/:id', component: EditComponent },
+  { path: 'list', component: ListComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ExempleComponent } from './components/exemple/exemple.component';
 import { MaterialModule } from './shared/material.module';
+import { ListComponent } from './components/list/list.component';
+import { EditComponent } from './components/edit/edit.component';
+import { CreateComponent } from './components/create/create.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, ExempleComponent],
+  declarations: [AppComponent, ExempleComponent, ListComponent, EditComponent, CreateComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
